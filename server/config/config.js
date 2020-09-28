a976e1dc59e5c16e6595837b197603019ad5c3be
Matias25Pinto <matias25pinto@gmail.com>
2020-09-28 18:37:21 -0400

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://matias25pinto:aspire5734z@cluster0.hmxoq.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
